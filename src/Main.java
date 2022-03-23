b68711e9246834684daba863a08bf6ffe1cb1bbe
Romanussss <vladroman1257@gmail.com>
2022-03-23 12:53:39 +0200

    TEMA

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,53 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello");
+        System.out.println("Vlad");
+
+       int A = 10;
+       int B = 5;
+
+       System.out.println(A+B);
+
+       float F = 8;
+       float D = 20;
+
+        System.out.println(F/D);
+
+        float G = -5;
+        float E = 8;
+        float C = 6;
+
+        System.out.println(G+E*C);
+
+        int X = 55;
+        int Y = 9;
+        float Z = 9;
+
+        int result = X+Y;
+        double result2 = result % Z;
+        System.out.println(result%Z);
+
+        double A1 = 20;
+        double B1 = -3;
+        double C1 = 5;
+        double D1 = 8;
+
+        System.out.println(A1+B1*C1/D1);
+
+        double A2 = 5;
+        double B2 = 15;
+        double C2 = 3;
+        double D2 = 2;
+        double E2 = -8;
+        double F2 = 3;
+        System.out.println(A2+B2/C2*D2-E2%F2);
+
+
+
+
 
 
     }
